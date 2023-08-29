134c1fc952c1b2f9fd3156474fb0d1be0003345c
alvcode <alvcode@ya.ru>
2023-08-29 23:42:01 +0300

    what's happens here 1

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,11 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->increments('id');
+            $table->bigInteger('phone_number')->nullable()->unique('user_phone_number_idx');
+            $table->string('email')->nullable()->unique('user_email_idx');
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
